18b7ed4e8a426ae6902e37220c21980338dad095
sergio <djsdm99@gmail.com>
2019-11-22 13:58:25 +0100

    Funcionamiento app acabada

--- lib/providers/peliculas_providers.dart
+++ lib/providers/peliculas_providers.dart
@@ -87,6 +87,19 @@ class PeliculasProvider {
 
   }
 
+  Future<List<Pelicula>> buscarPelicula(String query) async {
+    final url = Uri.https(_url, "3/search/movie",
+        {
+          'api_key': _apikey, 
+          'language': _language,
+          'query' : query
+        });
+
+    return await _procesarRespuesta(url);
+  }
+
+
+
 
 
 }
--- lib/search/search_delegate.dart
+++ lib/search/search_delegate.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:pelisapp/models/pelicula_model.dart';
+import 'package:pelisapp/providers/peliculas_providers.dart';
 
 class DataSearch extends SearchDelegate {
   final peliculas = ['Ironman','Spiderman', 'Aquaman', 'Batman', 'Superman', 'Cocoman','Shazam'];
 
   final peliculasRecientes = ['Spiderman', 'Capitan America'];
-
+  final peliculasProvider = new PeliculasProvider();
   String seleccion;
 
   @override
@@ -40,6 +42,7 @@ class DataSearch extends SearchDelegate {
       child: Container(
         height: 200,
         width: 200,
+        color: Colors.red,
         child: Text(seleccion),
       ),
     );
@@ -47,25 +50,47 @@ class DataSearch extends SearchDelegate {
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    final listaSugerida = (query.isEmpty)
-        ? peliculasRecientes
-        : peliculas
-            .where((p) => p.toLowerCase().startsWith(query.toLowerCase()))
-            .toList();
-    // Son las sugerencias que aparecen cuando escribres
-    return ListView.builder(
-      itemCount: listaSugerida.length,
-      itemBuilder: (context, i) {
-        return ListTile(
-          leading: Icon(Icons.movie),
-          title: Text(listaSugerida[i]),
-          subtitle: Text("ESE"),
-          onTap: () {
-            seleccion = listaSugerida[i];
-            showResults(context);
-          },
-        );
+
+    if(query.isEmpty){
+      return Container();
+    }
+
+    return FutureBuilder(
+      future: peliculasProvider.buscarPelicula(query),
+      builder: (context, AsyncSnapshot<List<Pelicula>> snapshot){
+        if(snapshot.hasData){
+
+          final pelis = snapshot.data;
+          return ListView(
+            children: pelis.map((p) {
+              return ListTile(
+                leading: FadeInImage(
+                  image: NetworkImage(p.getPosterImg(),
+                  ),
+                  placeholder: AssetImage("assets/img/no-image.jpg"),
+                  width: 50,
+                  fit: BoxFit.contain,
+                ),
+                title: Text(p.title),
+                subtitle: Text(p.originalTitle),
+                onTap: (){
+                  close(context,null);
+                  p.uniqueId = '';
+                  Navigator.of(context).pushNamed('detalle', arguments: p);
+                },
+              );
+            }
+          ).toList(),
+          );
+        }else{
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+
       },
     );
+    
   }
 }
